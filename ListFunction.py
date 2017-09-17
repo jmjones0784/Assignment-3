93f040ea97d34ed6119b18c7790c70a2f24c37bd
jmjones0784 <jamesmjones84@yahoo.com>
2017-09-16 23:10:57 -0400

    Base files
    Base files

--- ListFunction.py
+++ ListFunction.py
@@ -1,1 +1,10 @@
 import arcpy
+
+fc = r'C:\Student\AS.430.606\Assignment3\Assignment3\Newark.gdb\Schools'
+
+fields = arcpy.ListFields(fc)
+
+for f in fields:
+    print("Field Name: " + str(f))
+    print("Field Type:" + str(f.type()))
+    print("Field Length: " + str(f.length()))
