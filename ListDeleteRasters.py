c1601948c642257d994578c91c44dea144d7a638
jmjones0784 <jamesmjones84@yahoo.com>
2017-09-17 13:32:24 -0400

    Created List and Deleted Script
    Created List and Deleted Script

---
+++ ListDeleteRasters.py
@@ -0,0 +1,14 @@
+import arcpy
+from arcpy import env
+
+env.workspace = r'C:\Student\AS.430.606\Assignment3\Assignment3\Newark.gdb'
+
+rasters = arcpy.ListRasters("*")
+
+for ras in rasters:
+    if ras.startswith("N"):
+        raster = os.path.join(env.workspace, ras)
+        print(ras)
+        arcpy.Delete_management(raster)
+
+print("Complete")
--- ListFunction.py
+++ ListFunction.py
@@ -5,6 +5,7 @@ fc = r'C:\Student\AS.430.606\Assignment3\Assignment3\Newark.gdb\Schools'
 fields = arcpy.ListFields(fc)
 
 for f in fields:
-    print("Field Name: " + str(f))
-    print("Field Type:" + str(f.type()))
-    print("Field Length: " + str(f.length()))
+    print("Field Name:      {0}".format(f.name))
+    print("Field Type:      {0}".format(f.type))
+    print("Field Length:    {0}".format(f.length))
+    print("\n")
